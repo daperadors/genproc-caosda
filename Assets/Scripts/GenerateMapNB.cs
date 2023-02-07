fc8d85073ee6668414137ce4f8f7701f8531967e
david <david@caosda.es>
2023-02-07 16:08:38 +0100

    instantiate ores

--- Assets/Scripts/GenerateMapNB.cs
+++ Assets/Scripts/GenerateMapNB.cs
@@ -5,6 +5,7 @@ public class GenerateMapNB : MonoBehaviour
     [Header("Map var")]
     [SerializeField] private GameObject[] m_BlockList;
     [SerializeField] private float[] m_BlockListThreshold;
+    [SerializeField] private GameObject[] m_MineralList;
     [SerializeField] private int m_Seed = 98742364;
     [SerializeField] private float m_Scale = 35f;
     [SerializeField] private float m_Frequency = 20f;
@@ -13,7 +14,6 @@ public class GenerateMapNB : MonoBehaviour
     [SerializeField] private int m_SizeX = 50;
     [SerializeField] private int m_SizeZ = 50;
 
-    private GameObject m_ActualBlock;
     private Vector3 m_Position;
 
     [Header("Trees var")]
@@ -23,14 +23,13 @@ public class GenerateMapNB : MonoBehaviour
     [SerializeField] private float m_TreeFrequency = 10f;
     [Range(0f, 1f)]
     [SerializeField] private float m_TreeThreshold = 0.5f;
-    [SerializeField, Range(10f, 0f)] private int m_TreeDensity;
+    [SerializeField, Range(20f, 0f)] private int m_TreeDensity;
 
     private int[,] m_Terrain;
 
     private void Start()
     {
         m_Position = transform.position;
-        m_ActualBlock = m_BlockList[2];
         GenerateProceduralMapNoBiomas();
         GenerateTrees();
     }
@@ -55,10 +54,49 @@ public class GenerateMapNB : MonoBehaviour
                         break;
                     }
                 }
+                if (indiceBloque == 0)
+                {
+                    float rand = Random.Range(0, 20);
 
-                GameObject block = Instantiate(m_BlockList[indiceBloque], transform);
-                block.transform.position = new Vector3(m_Position.x + x, y, m_Position.z + z);
-                m_Terrain[x, z] = y;
+                    if (rand == 1)
+                    {
+                        GameObject block = Instantiate(m_MineralList[0], transform);
+                        block.transform.position = new Vector3(m_Position.x + x, y, m_Position.z + z);
+                        GameObject block2 = Instantiate(m_BlockList[0], transform);
+                        block2.transform.position = new Vector3(m_Position.x + x, y - 1, m_Position.z + z);
+                    }
+                    else if (rand >= 5 && rand <= 8)
+                    {
+                        GameObject block = Instantiate(m_MineralList[1], transform);
+                        block.transform.position = new Vector3(m_Position.x + x, y, m_Position.z + z);
+                        GameObject block2 = Instantiate(m_BlockList[0], transform);
+                        block2.transform.position = new Vector3(m_Position.x + x, y - 1, m_Position.z + z);
+                    }
+                    else if (rand == 9 || rand == 12)
+                    {
+                        GameObject block = Instantiate(m_MineralList[2], transform);
+                        block.transform.position = new Vector3(m_Position.x + x, y, m_Position.z + z);
+                        GameObject block2 = Instantiate(m_BlockList[0], transform);
+                        block2.transform.position = new Vector3(m_Position.x + x, y - 1, m_Position.z + z);
+                    }
+                    else
+                    {
+                        GameObject block = Instantiate(m_BlockList[0], transform);
+                        block.transform.position = new Vector3(m_Position.x + x, y, m_Position.z + z);
+                        GameObject block2 = Instantiate(m_BlockList[0], transform);
+                        block2.transform.position = new Vector3(m_Position.x + x, y - 1, m_Position.z + z);
+                    }
+                }
+                else
+                {
+                    GameObject block = Instantiate(m_BlockList[indiceBloque], transform);
+                    GameObject block2 = Instantiate(m_BlockList[indiceBloque], transform);
+
+                    block.transform.position = new Vector3(m_Position.x + x, y, m_Position.z + z);
+                    block2.transform.position = new Vector3(m_Position.x + x, y - 1, m_Position.z + z);
+                    if (valorPerlin > m_TreeThreshold) m_Terrain[x, z] = y;
+                }
+
             }
         }
     }
@@ -75,8 +113,11 @@ public class GenerateMapNB : MonoBehaviour
                 {
                     if (Random.Range(0, m_TreeDensity) == 1)
                     {
-                        GameObject tree = Instantiate(m_TreeList[0], transform);
-                        tree.transform.position = new Vector3(x, m_Terrain[x, y] + 1, y);
+                        if (m_Terrain[x, y] > m_TreeThreshold)
+                        {
+                            GameObject tree = Instantiate(m_TreeList[0], transform);
+                            tree.transform.position = new Vector3(x, m_Terrain[x, y] + 1, y);
+                        }
                     }
                 }
 
